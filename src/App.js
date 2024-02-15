1e811f10dc08342a2933d0031ae90cb69a8ef76c
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2024-02-15 16:15:16 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Header from "./components/main/Header";
+import Footer from "./components/main/Footer";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import Services_detail from "./pages/Services_detail";
+import Service from "./pages/Service";
+import { useEffect } from "react";
+import Services from "./pages/Services";
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact_us" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/s_detail" element={<Services_detail />} />
+        <Route path="/companies" element={<Service />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
